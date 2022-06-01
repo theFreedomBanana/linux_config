e9db099ae3424aa2611b63ec9b6e163f9bd80f11
Ben <benjamin.mahmoudi@gmail.com>
2022-06-01 16:27:03 +0200

    feat: echo starting message at the beginning of every function

--- install.sh
+++ install.sh
@@ -7,6 +7,7 @@
 
 #!/bin/bash -e
 generateSshKey() {
+	echo "About to generate SSH key"
 	ssh-keygen -t rsa -b 4096
 	echo "A new SSH key pair has been added to ${HOME}/.ssh/id_rsa. Add it to your github profile."
 	echo "Enter 'y' when it's done:"
@@ -17,6 +18,7 @@ generateSshKey() {
 }
 
 addCustomConfig() {
+	echo "About to fetch custom config from github repo"
 	git clone git@github.com:theFreedomBanana/config.git
 	mv .gitconfig ~/
 	mv .commands_and_aliases.sh ~/
@@ -24,28 +26,33 @@ addCustomConfig() {
 }
 
 installMatchaThemes() {
+	echo "About to install Matcha theme"
 	apt-get install gtk2-engines-murrine gtk2-engines-pixbuf -y
 	git clone https://github.com/vinceliuice/matcha.git
 	./install.sh
 }
 
 installFzf() {
+	echo "About to install FZF"
 	git clone --depth 1 https://github.com/junegunn/fzf.git
 	cd ~/.fzf/
 	./install
 }
 
 installNvm() {
+	echo "About to install NVM"
 	curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
 	nvm install --lts
 }
 
 installNgrok() {
+	echo "About to install Ngrok"
 	tar xvzf ~/Downloads/ngrok-stable-linux-amd64.tgz -C /usr/local/bin
 	curl -s https://ngrok-agent.s3.amazonaws.com/ngrok.asc | tee /etc/apt/trusted.gpg.d/ngrok.asc >/dev/null && echo "deb https://ngrok-agent.s3.amazonaws.com buster main" | tee /etc/apt/sources.list.d/ngrok.list && apt update && apt install ngrok
 }
 
 installVscode() {
+	echo "About to install VSCode and extensions"
 	apt install -y software-properties-common apt-transport-https wget
 	wget -q https://packages.microsoft.com/keys/microsoft.asc -O- | apt-key add -
 	add-apt-repository "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main"
@@ -67,27 +74,32 @@ installVscode() {
 }
 
 installTerminator() {
+	echo "About to install Terminator"
 	apt install terminator -y
 	mv terminator/config ~/.config/terminator/config
 }
 
 installChrome() {
+	echo "About to install Chrome"
 	wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 	dpkg -i google-chrome-stable_current_amd64.deb
 }
 
 installZoom() {
+	echo "About to install Zoom"
 	wget https://zoom.us/client/latest/zoom_amd64.deb
 	apt install ./zoom_amd64.deb -y
 }
 
 installDiscord() {
+	echo "About to install Discord"
 	apt install gdebi-core wget
 	wget -O discord.deb "https://discordapp.com/api/download?platform=linux&format=deb"
 	gdebi ./discord.deb
 }
 
 updateGnomeDock() {
+	echo "About to add favorite in Gnome Dock"
 	gsettings set org.gnome.shell favorite-apps "['google-chrome.desktop', 'firefox.desktop', 'thunderbird.desktop', 'slack.desktop', 'code.desktop', 'terminator.desktop', 'org.gnome.Nautilus.desktop', 'gnome-system-monitor_gnome-system-monitor.desktop', 'htop.desktop', 'org.gnome.tweaks.desktop', 'vokoscreen.desktop', 'org.gnome.Software.desktop', 'yelp.desktop']"
 	gsettings set org.gnome.shell.extensions.dash-to-dock dock-position RIGHT
 }
