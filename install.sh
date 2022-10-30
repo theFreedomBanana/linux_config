332fb4ffd1a0da98bab13668549d465595d6ed6d
Ben <benjamin.mahmoudi@gmail.com>
2022-10-30 15:18:17 +0100

    Execute Google-Chrome install as sudo

--- install.sh
+++ install.sh
@@ -108,7 +108,7 @@ installChrome() {
 INSTALLING CHROME
 ##########################################"
 	wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-	dpkg -i google-chrome-stable_current_amd64.deb
+	sudo dpkg -i google-chrome-stable_current_amd64.deb
 }
 
 installZoom() {
